801b5488fb7c408c11ef0d4820bc7b41926d41a1
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-04-27 00:27:08 +0300

    22 rolic

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = (props) => {
                   <Routes>
                       <Route path='/news' element = {<News />} />
                       <Route path='/profile' element = {<Profile />} />
-                      <Route path='/dialogs' element = {<Dialogs />} />
+                      <Route path='/dialogs/*' element = {<Dialogs />} />
                       <Route path='/music' element = {<Music />} />
                       <Route path='/settings' element = {<Settings />} />
                   </Routes>
