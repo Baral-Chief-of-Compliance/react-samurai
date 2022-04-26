ecefd72a926c906def4486ffb10800376da216f3
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-04-26 23:53:06 +0300

    21 rolic

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,35 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return(
-      <div className={s.content}>
-          <div>Диалоги</div>
+      <div className={s.dialogs}>
+          <div className={s.dioalogsItems}>
+              <div className={s.dialog}>
+                  кто бля
+              </div>
+              <div className={s.dialog}>
+                  я бля
+              </div>
+              <div className={s.dialog+' '+s.active}>
+                  кого ты выберешь
+              </div>
+              <div className={s.dialog}>
+                  нас
+              </div>
+              <div className={s.dialog}>
+                  или
+              </div>
+              <div className={s.dialog}>
+                  их
+              </div>
+              <div className={s.dialog}>
+                  а?
+              </div>
+          </div>
+          <div className = {s.messages}>
+              <div className={s.message}>КАК</div>
+              <div className={s.message}>там</div>
+              <div className={s.message}>с деньгами</div>
+          </div>
       </div>
   )
 }
