5e055896832f84bfaba695d091c81933847d7ff5
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-05-08 22:42:07 +0300

    08.05.2022

--- src/App.js
+++ src/App.js
@@ -12,32 +12,11 @@ import './App.css';
 
 const App = (props) => {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <div className='app-wrapper'>
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-            <Router>
-                <Routes>
-                    <Route path='/dialogs' element = {<Dialogs />} />
-                    <Route path='/profile' element = {<Profile />} />
-                </Routes>
-            </Router>
-        </div>
-      </div>
-=======
-=======
->>>>>>> 801b5488fb7c408c11ef0d4820bc7b41926d41a1
       <Router>
           <div className='app-wrapper'>
               <Header />
               <Navbar NavbarProps={props.state.sidebar}/>
               <div className="app-wrapper-content">
-<<<<<<< HEAD
->>>>>>> 801b5488fb7c408c11ef0d4820bc7b41926d41a1
-=======
->>>>>>> 801b5488fb7c408c11ef0d4820bc7b41926d41a1
 
                   <Routes>
                       <Route path='/news' element = {<News />} />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import s from './Navbar.module.css';
 import {NavLink} from "react-router-dom";
-<<<<<<< HEAD
 import FriendsBar from './FriendsBar/FriendsBar.jsx'
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 801b5488fb7c408c11ef0d4820bc7b41926d41a1
->>>>>>> ac6b22f41b4e4ee99341e3349f4113a83c3126a5
 
 
->>>>>>> 801b5488fb7c408c11ef0d4820bc7b41926d41a1
 
 
 const Navbar = (props) => {
@@ -22,29 +13,13 @@ const Navbar = (props) => {
           <NavLink to='/news'>Новости</NavLink>
       </div>
       <div className = {s.item}>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <NavLink to ="/profile">Профиль</NavLink>
-      </div>
-      <div className ={s.item}>
-        <NavLink to ="/dialogs">Сообщения</NavLink>
-=======
         <NavLink to='/profile'>Профиль</NavLink>
       </div>
       <div className ={s.item}>
-=======
-        <NavLink to='/profile'>Профиль</NavLink>
-      </div>
-      <div className ={s.item}>
->>>>>>> 801b5488fb7c408c11ef0d4820bc7b41926d41a1
         <NavLink to='/dialogs'>Сообщения</NavLink>
       </div>
       <div className={s.item}>
           <NavLink to='/music'>Музыка</NavLink>
-<<<<<<< HEAD
->>>>>>> 801b5488fb7c408c11ef0d4820bc7b41926d41a1
-=======
->>>>>>> 801b5488fb7c408c11ef0d4820bc7b41926d41a1
       </div>
       <div className = {`${s.item} ${s.active}`}>
         <NavLink to="/settings">Настройки</NavLink>
